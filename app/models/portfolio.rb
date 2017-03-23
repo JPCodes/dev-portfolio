c1ef6bb7e3eb2c944026cb020be79830c7d356fe
Jim Padilla <jp_code2013@yahoo.com>
2017-03-23 11:03:51 -0700

    Add portfolio image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
+  after_initialize :set_defaults
+
 
   # Lame, vanilla syntax
   def self.angular
@@ -9,4 +11,10 @@ class Portfolio < ApplicationRecord
   # Alternate, cooler syntax
     # Any name for scope, ex: could be :ruby_on_rails_stuff
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+  def set_defaults
+    # ||= means if main_image nil then set to "string"
+    self.main_image ||=  "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
