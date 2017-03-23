32c4bc8b827d7736a4342065675a1102f0b94b67
Jim Padilla <jp_code2013@yahoo.com>
2017-03-23 10:49:23 -0700

    add custom scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
+
+  # Lame, vanilla syntax
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  # Alternate, cooler syntax
+    # Any name for scope, ex: could be :ruby_on_rails_stuff
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 end
