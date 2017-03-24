cccc8b980dc46bee65d0eac69ddaa6d3a8fd1182
Jim Padilla <jp_code2013@yahoo.com>
2017-03-23 18:15:03 -0700

    Add session tracker for referrer link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist # Concerns file
+
+  before_action :set_source
+
+  def set_source
+    # source param can be called anything
+    session[:source] = params[:q] if params[:q]
+  end
 end
