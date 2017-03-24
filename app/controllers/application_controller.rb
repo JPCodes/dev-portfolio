2238285c997f8bdafb8491d0ff77edee2f212e47
Jim Padilla <jp_code2013@yahoo.com>
2017-03-23 18:23:51 -0700

    refactor app controller to include setsource concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist # Concerns file
-
-  before_action :set_source
-
-  def set_source
-    # source param can be called anything
-    session[:source] = params[:q] if params[:q]
-  end
+  include SetSource
 end
