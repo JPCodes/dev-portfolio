1ab820efc668e0d739eee7107c019b568a4d7268
--global <jp_code2013@yahoo.com>
2017-03-26 14:15:34 -0700

    Fix login helper bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   # No parenthesis needed for params
-  def login_helper style
+    # Empty string is default
+  def login_helper style = ''
      if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       (link_to "Login", new_user_session_path, class: style)
