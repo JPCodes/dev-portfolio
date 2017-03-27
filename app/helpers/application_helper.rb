8590e2c93614b953b645256d630b782cc896f9f1
--global <jp_code2013@yahoo.com>
2017-03-26 20:29:35 -0700

    Use heredoc for nav (will refactor), add helper to know which link is active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # No parenthesis needed for params
-    # Empty string is default
+    # Empty string is default argument
   def login_helper style = '', separator = ''
      if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
@@ -21,5 +21,22 @@ module ApplicationHelper
 
   def copyright_generator
     ViewFooterCopyright::Renderer.copyright 'Jim Padilla', 'All rights reserved'
+  end
+
+  # Maybe add blank tag type to delete app-nav class
+  def nav_generator style, tag_type
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blogs</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+NAV
+
+  nav_links.html_safe
+  end
+
+  def active? path
+    "active".html_safe if current_page? path
   end
 end
