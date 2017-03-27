bcba92ec2fa0896183480db09f0427885e7947f2
--global <jp_code2013@yahoo.com>
2017-03-26 20:38:27 -0700

    Refactor nav, active helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,17 +23,39 @@ module ApplicationHelper
     ViewFooterCopyright::Renderer.copyright 'Jim Padilla', 'All rights reserved'
   end
 
-  # Maybe add blank tag type to delete app-nav class
-  def nav_generator style, tag_type
-nav_links = <<NAV
-<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
-<#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
-<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
-<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blogs</a></#{tag_type}>
-<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
-NAV
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
 
-  nav_links.html_safe
+  def nav_generator style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
   end
 
   def active? path
